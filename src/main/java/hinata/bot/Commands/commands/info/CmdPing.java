703675d47e55612aacf4a3c025c2ca0b6b955db7
Drag0nDev <lennvh046@gmail.com>
2021-08-21 19:07:12 +0200

    adding the ping command

--- src/main/java/hinata/bot/Commands/commands/info/CmdPing.java
+++ src/main/java/hinata/bot/Commands/commands/info/CmdPing.java
@@ -1,13 +1,17 @@
-package hinata.bot.Commands.commands;
+package hinata.bot.Commands.commands.info;
 
 import com.github.rainestormee.jdacommand.CommandAttribute;
 import com.github.rainestormee.jdacommand.CommandDescription;
 import hinata.bot.Commands.Command;
 import hinata.bot.Hinata;
+import hinata.bot.constants.Colors;
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.TextChannel;
+
+import java.util.Date;
 
 @CommandDescription(
         name = "Ping",
@@ -28,12 +32,19 @@ public class CmdPing implements Command {
     }
 
     @Override
-    public void run(Guild guild, TextChannel tc, Message msg, Member member, String... args) {
+    public void execute(Message msg, Object... args) {
+        Date now = new Date();
+        Date sent = new Date(msg.getTimeCreated().toInstant().toEpochMilli());
 
-    }
+        long ping = now.getTime() - sent.getTime();
 
-    @Override
-    public void execute(Message object, Object... args) {
+        EmbedBuilder embed = new EmbedBuilder();
 
+        embed.setTitle("Ping")
+                .setDescription(ping + "ms")
+                .setColor(Colors.NORMAL.getCode())
+                .setTimestamp(now.toInstant());
+
+        msg.getChannel().sendMessageEmbeds(embed.build()).queue();
     }
 }
