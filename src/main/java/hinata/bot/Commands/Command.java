a6b306c6df20a58a87143daf69fe5b8f04fb6f3a
Drag0nDev <lennvh046@gmail.com>
2021-09-11 22:33:17 +0200

    logging for possible problems with slash commands

--- src/main/java/hinata/bot/Commands/Command.java
+++ src/main/java/hinata/bot/Commands/Command.java
@@ -1,6 +1,7 @@
 package hinata.bot.Commands;
 
 import com.github.rainestormee.jdacommand.AbstractCommand;
+import hinata.bot.Hinata;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.Message;
@@ -13,12 +14,16 @@ import java.util.Arrays;
 
 public interface Command extends AbstractCommand<Message> {
 
-    default void executeSlash(SlashCommandEvent event) {
+    default void executeSlash(Hinata bot, SlashCommandEvent event) {
         if (event.getMember() == null)
             return;
 
-        event.deferReply().queue();
-        run(event.getGuild(), event.getTextChannel(), event.getMember(), event, event.getHook());
+        try {
+            event.deferReply().queue();
+            run(event.getGuild(), event.getTextChannel(), event.getMember(), event, event.getHook());
+        } catch (Exception exception) {
+            bot.getLogger().error(exception.getMessage());
+        }
     }
 
     CommandData slashInfo();
