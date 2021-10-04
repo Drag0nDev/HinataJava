db369cbc3fb4ddcd272aed7b9c872b8b8768b67d
Drag0nDev <lennvh046@gmail.com>
2021-10-04 21:55:45 +0200

    Split up calls: DO NOT PUT THEM TOGETHER IN AN ENUM. this will result in a single gif and not random gifs

---
+++ src/main/java/hinata/bot/constants/ApiCalls.java
@@ -0,0 +1,73 @@
+package hinata.bot.constants;
+
+import hinata.bot.util.apiHandling.APOD;
+import hinata.bot.util.apiHandling.JsonBodyHandler;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.concurrent.ExecutionException;
+import java.util.function.Supplier;
+
+public enum ApiCalls {
+    //SFW api calls
+    //gifs
+    BAKA    ("https://nekos.life/api/v2/img/baka",      false),
+    CUDDLE  ("https://nekos.life/api/v2/img/cuddle",    false),
+    HUG     ("https://nekos.life/api/v2/img/hug",       false),
+    KISS    ("https://nekos.life/api/v2/img/kiss",      false),
+    PAT     ("https://nekos.life/api/v2/img/pat",       false),
+    POKE    ("https://nekos.life/api/v2/img/poke",      false),
+    SLAP    ("https://nekos.life/api/v2/img/slap",      false),
+
+    //images
+    KITSUNE     ("https://nekos.life/api/v2/img/fox_girl",  false),
+    NEKO        ("https://nekos.life/api/v2/img/neko",      false),
+
+    //NSFW api calls
+    //gifs
+    SPANK   ("https://nekos.life/api/v2/img/spank",     true),
+
+    //images
+    ERO     ("https://nekos.life/api/v2/img/eron",      true),
+    FUTA    ("https://nekos.life/api/v2/img/futanari",  true),
+    HENTAI  ("https://nekos.life/api/v2/img/hentai",    true),
+    LEWD    ("https://nekos.life/api/v2/img/lewd",      true),
+    TRAP    ("https://nekos.life/api/v2/img/trap",      true)
+    ;
+
+    private final String apiCall;
+    private final boolean nsfw;
+
+    ApiCalls(String apiCall, boolean nsfw) {
+        this.apiCall = apiCall;
+        this.nsfw = nsfw;
+    }
+
+    public boolean isNsfw(){
+        return this.nsfw;
+    }
+
+    public String get(){
+        var client = HttpClient.newHttpClient();
+
+        var request = HttpRequest.newBuilder(
+                        URI.create(this.apiCall))
+                .header("accept", "application/json")
+                .build();
+
+        var responseFuture = client.sendAsync(request, new JsonBodyHandler<>(APOD.class));
+
+        HttpResponse<Supplier<APOD>> response = null;
+        try {
+            response = responseFuture.get();
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        assert response != null;
+        return response.body().get().url;
+    }
+}
+
