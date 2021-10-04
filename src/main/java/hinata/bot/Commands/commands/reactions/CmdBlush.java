e5dcee28241b23ee0e52fad44958792376303c3a
Drag0nDev <lennvh046@gmail.com>
2021-10-04 21:54:45 +0200

    Added blush command

---
+++ src/main/java/hinata/bot/Commands/commands/reactions/CmdBlush.java
@@ -0,0 +1,85 @@
+package hinata.bot.Commands.commands.reactions;
+
+import com.github.rainestormee.jdacommand.CommandAttribute;
+import com.github.rainestormee.jdacommand.CommandDescription;
+import hinata.bot.Commands.Command;
+import hinata.bot.Hinata;
+import hinata.bot.constants.*;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.MessageBuilder;
+import net.dv8tion.jda.api.entities.*;
+import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
+import net.dv8tion.jda.api.interactions.InteractionHook;
+import net.dv8tion.jda.api.interactions.commands.build.CommandData;
+
+import java.time.ZonedDateTime;
+
+@CommandDescription(
+        name = "blush",
+        description = "Show a blushing gif",
+        triggers = {"blush"},
+        attributes = {
+                @CommandAttribute(key = "category", value = "reactions"),
+                @CommandAttribute(key = "usage", value = "[command | alias]"),
+                @CommandAttribute(key = "examples", value = "`h!blush"),
+        }
+)
+
+public class CmdBlush implements Command {
+    private Hinata bot;
+
+    private final CommandData slashInfo = new CommandData(this.getDescription().name(), this.getDescription().description());
+
+    public CmdBlush(Hinata bot) {
+        this.bot = bot;
+    }
+
+    @Override
+    public void run(Guild guild, TextChannel tc, Member member, SlashCommandEvent event, InteractionHook hook) {
+        String text = member.getAsMention() + " is blushing!";
+        EmbedBuilder embed = new EmbedBuilder()
+                .setColor(Colors.NORMAL.getCode())
+                .setImage(CustomReactions.blush())
+                .setFooter("Powered by lost hopes and dreams")
+                .setTimestamp(ZonedDateTime.now());
+
+        MessageBuilder message = new MessageBuilder().setContent(text)
+                .setEmbeds(embed.build());
+
+        hook.sendMessage(message.build()).queue();
+    }
+
+    @Override
+    public void execute(Message msg, Object... args) {
+        TextChannel tc = msg.getTextChannel();
+        Member executor = msg.getMember();
+
+        EmbedBuilder embed = new EmbedBuilder()
+                .setColor(Colors.NORMAL.getCode())
+                .setImage(CustomReactions.blush())
+                .setFooter("Powered by lost hopes and dreams")
+                .setTimestamp(ZonedDateTime.now());
+
+        String text = executor.getAsMention() + " is blushing!";
+
+        MessageBuilder message = new MessageBuilder().setContent(text)
+                .setEmbeds(embed.build())
+                .denyMentions(
+                        Message.MentionType.ROLE,
+                        Message.MentionType.EVERYONE,
+                        Message.MentionType.HERE
+                );
+
+        tc.sendMessage(message.build()).queue();
+    }
+
+    @Override
+    public CommandData slashInfo() {
+        return this.slashInfo;
+    }
+
+    @Override
+    public String getOptionName() {
+        return null;
+    }
+}
